ab344c170f0f7ac8eb376c0b7d3d59f96bcf8b7f
CodingSAK <minsik0211@gmail.com>
2023-08-01 17:31:17 +0900

    day9 v-bind

---
+++ learn-vue3/src/lectures/day9/App_Stylebind.vue
@@ -0,0 +1,26 @@
+<template>
+	<div :style="styleObject">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, culpa
+		ratione? Laborum beatae provident, obcaecati voluptate harum velit. Officiis
+		iusto nemo totam eligendi iure inventore natus aliquid veritatis recusandae
+		iste!
+	</div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+	setup() {
+		const styleObject = reactive({
+			color: 'red',
+			fontSize: '13px'
+		});
+		return {
+			styleObject
+		};
+	}
+};
+</script>
+
+<style lang="scss" scoped></style>
