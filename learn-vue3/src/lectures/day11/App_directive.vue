12492dea84078a18c5621bef721a516947132150
CodingSAK <minsik0211@gmail.com>
2023-08-03 13:54:00 +0900

    directive v-memo

---
+++ learn-vue3/src/lectures/day11/App_directive.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<p v-text="msg"></p>
+		<p>{{ msg }}</p>
+		<p>{{ htmlmsg }}</p>
+		<p v-html="htmlmsg"></p>
+		<p v-once>{{ msg }} !!!!</p>
+		<!-- cant modify F12 v-once -->
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+	setup() {
+		const msg = ref('hello');
+		const htmlmsg = ref('<strong>v-html!!</strong>');
+		return {
+			msg,
+			htmlmsg
+		};
+	}
+};
+</script>
+
+<style lang="scss" scoped></style>
